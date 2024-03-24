d4bc9ce23047ff23e3ca2b9353bd1d89516765c8
JoyRume <joy.rume@student.moringaschool.com>
2024-03-24 10:05:51 +0300

    completed

---
+++ app/migrations/versions/7d12fd642cd7_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 7d12fd642cd7
+Revises: 
+Create Date: 2024-03-15 16:51:29.826439
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7d12fd642cd7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
